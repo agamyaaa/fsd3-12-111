6d75e92e07754630cc281389855b081e54fa8838
Agamya Yadav <agamyayadav9@gmail.com>
2026-09-17 10:58:11 +0530

    labs 2 revised

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,11 +1,11 @@
- import {stat} from "fs/promises";
+import { stat } from "fs/promises";
 
- const fstat = await stat("file1.js");
+const fstat = await stat("file1.js");
 
- console.log("file.size", fstat.size, "bytes");
+console.log("file.size", fstat.size, "bytes");
 console.log(`is file: ${fstat.isFile()}`);
 console.log(`is folder: ${fstat.isDirectory()}`);
 console.log(`is syslink: ${fstat.isSymbolicLink()}`);
 console.log(`is syslink: ${fstat.birthtime}`);
 console.log(`last used": ${fstat.atime}`);
-
+//completed
